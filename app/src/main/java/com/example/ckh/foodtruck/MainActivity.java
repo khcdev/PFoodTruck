3f22a601da0cb0a8bd665fcedde7beb927fc707a
Hoon_C <chakh9601@gmail.com>
2016-09-03 15:28:18 +0900

    first commit - base layout working TEST2

---
+++ app/src/main/java/com/example/ckh/foodtruck/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ckh.foodtruck;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_main);
+    }
+}
