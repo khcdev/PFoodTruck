f6fd896e66bd5dbd3a3646552a486ee4bd514a61
Hoon_C <chakh9601@gmail.com>
2016-09-08 14:49:14 +0900

    commit test3 - gradle sync

--- app/src/main/java/com/example/ckh/foodtruck/MainActivity.java
+++ app/src/main/java/com/example/ckh/foodtruck/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.ckh.foodtruck;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_main);
+        setContentView(R.layout.activity_main);
+
+        View.OnClickListener titlelayout_listener = new btnClickLister();
+
+        Button imgbtn_seller = (Button) findViewById(R.id.titlelayout_start_seller);
+        Button imgbtn_user = (Button)findViewById(R.id.titlelayout_start_user);
+        imgbtn_seller.setOnClickListener(titlelayout_listener);
+
+    }
+
+    class btnClickLister implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            switch(v.getId()){
+                case R.id.titlelayout_start_seller:
+                    Intent i = new Intent();
+                    //startActivity();
+                    break;
+                case R.id.titlelayout_start_user:
+                    break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/ckh/foodtruck/User_Login.java
@@ -0,0 +1,10 @@
+package com.example.ckh.foodtruck;
+
+import android.app.Activity;
+
+/**
+ * Created by Ckh on 2016-09-08.
+ */
+public class User_Login extends Activity{
+
+}
