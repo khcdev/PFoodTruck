d70066989e81cb3b9d6b5fc36d4a85636753e3d5
Hoon_C <chakh9601@gmail.com>
2016-09-12 14:43:45 +0900

    hot fix - min sdk modi

--- app/src/main/java/com/example/ckh/foodtruck/User_Login.java
+++ app/src/main/java/com/example/ckh/foodtruck/User_Login.java
@@ -3,6 +3,7 @@ package com.example.ckh.foodtruck;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 
@@ -36,7 +37,7 @@ import java.util.Arrays;
  * ->kakao,facebook API이용
  */
 public class User_Login extends Activity{
-    private SessionCallback mKakaocallback;
+    //private SessionCallback mKakaocallback;
     private CallbackManager callbackManager;
     UserData UserData;
 
@@ -49,7 +50,6 @@ public class User_Login extends Activity{
         ImageView imgbtn_kkoLogin=(ImageView) findViewById(R.id.usermain_btn_kkoLogin);
         ImageView imgbtn_fbLogin=(ImageView) findViewById(R.id.usermain_btn_fbLogin);
 
-
         class btnClickListener implements View.OnClickListener{
             @Override
             public void onClick(View v) {
@@ -59,7 +59,7 @@ public class User_Login extends Activity{
                         break;
                     case R.id.usermain_btn_kkoLogin :
                         UserData = new UserData();
-                        isKakaoLogin();
+                        //isKakaoLogin();
                         Toast.makeText(User_Login.this,UserData.getuser_id(),Toast.LENGTH_LONG).show();
                         break;
                     case R.id.usermain_btn_fbLogin :
@@ -77,63 +77,7 @@ public class User_Login extends Activity{
 
 
     }
-    private void isKakaoLogin() {
-        // 카카오 세션을 오픈한다
-        mKakaocallback = new SessionCallback();
-        com.kakao.auth.Session.getCurrentSession().addCallback(mKakaocallback);
-        com.kakao.auth.Session.getCurrentSession().checkAndImplicitOpen();
-        com.kakao.auth.Session.getCurrentSession().open(AuthType.KAKAO_TALK_EXCLUDE_NATIVE_LOGIN, User_Login.this);
-    }
-    private class SessionCallback implements ISessionCallback {
-        @Override
-        public void onSessionOpened() {
-            Log.d("TAG" , "세션 오픈됨");
-            // 사용자 정보를 가져옴, 회원가입 미가입시 자동가입 시킴
-            KakaorequestMe();
-        }
 
-        @Override
-        public void onSessionOpenFailed(KakaoException exception) {
-            if(exception != null) {
-                Log.d("TAG" , exception.getMessage());
-            }
-        }
-    }
-    /**
-     * 사용자의 상태를 알아 보기 위해 me API 호출을 한다.
-     */
-    protected void KakaorequestMe() {
-        UserManagement.requestMe(new MeResponseCallback() {
-            @Override
-            public void onFailure(ErrorResult errorResult) {
-                int ErrorCode = errorResult.getErrorCode();
-                int ClientErrorCode = -777;
-
-                if (ErrorCode == ClientErrorCode) {
-                    Toast.makeText(getApplicationContext(), "카카오톡 서버의 네트워크가 불안정합니다. 잠시 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Log.d("TAG" , "오류로 카카오로그인 실패 ");
-                }
-            }
-
-            @Override
-            public void onSessionClosed(ErrorResult errorResult) {
-                Log.d("TAG" , "오류로 카카오로그인 실패 ");
-            }
-
-            @Override
-            public void onSuccess(UserProfile userProfile) {
-                UserData.setuser_id(String.valueOf(userProfile.getId()));
-                UserData.setUser_name(userProfile.getNickname());
-                //setLayoutText();
-            }
-
-            @Override
-            public void onNotSignedUp() {
-                // 자동가입이 아닐경우 동의창
-            }
-        });
-    }
 //fragment layout의 도입
     /*private void setLayoutText(){
         tv_user_id.setText(userId);
@@ -146,9 +90,11 @@ public class User_Login extends Activity{
     }*/
     private void isLoginFacebook(){
         FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         CallbackManager callbackManager = CallbackManager.Factory.create();
-        Toast.makeText(this, "access!", Toast.LENGTH_LONG).show();
+
         LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile","email"));
+        Toast.makeText(this, "access!", Toast.LENGTH_LONG).show();
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(final LoginResult loginResult) {
